19ae6ec12357f05d97ac6faf23d3e5cf957bd0d4
2319484461@qq.com <2319484461@qq.com>
2018-03-19 16:52:09 +0800

    优化地图导航； 增加打卡签到功能。

--- EpathmapSDK.podspec
+++ EpathmapSDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'EpathmapSDK'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = '释途室内地图导航SDK'
 
 
--- EpathmapSDK/EpathmapSDK.framework/Headers/EpathmapLocationManger.h
+++ EpathmapSDK/EpathmapSDK.framework/Headers/EpathmapLocationManger.h
@@ -6,14 +6,13 @@
 //  Copyright © 2017年 Epathmap. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
-@class EpathmapLocationManger;
-@class EpathLocationInfo;
-@protocol EpathmapLocationDelegate <NSObject>
+#import "EpathLocationInfo.h"
 
-- (void)ipsmapLocationManager:(EpathmapLocationManger *_Nullable)manager didUpdateLocation:(EpathLocationInfo *_Nullable)location;
+@protocol EpathmapLocationDelegate;
 
-@end
+typedef void(^EpathMapViewSignBlock)(CGFloat distance,BOOL isSign);
 
 @interface EpathmapLocationManger : NSObject
 
@@ -21,9 +20,19 @@
 
 @property (nonatomic, assign)NSUInteger locationTimeOut;
 
+@property (nonatomic, copy) EpathMapViewSignBlock _Nullable signBlock;
+
+- (void)signWithMapId:(nonnull NSString *)mapId targetId:(nonnull NSString *)targetId signDistance:(CGFloat)distance resultBlock:(EpathMapViewSignBlock _Nullable )signBlock;
+
++(EpathmapLocationManger *_Nullable)shareInstance;
 
 - (void)startLocationEngine:(nonnull NSString *)mapId;
 
 - (void)stopLocatingEngine;
+
+@end
+@protocol EpathmapLocationDelegate <NSObject>
+
+- (void)ipsmapLocationManager:(EpathmapLocationManger *_Nullable)manager didUpdateLocation:(EpathLocationInfo *_Nullable)location;
 
 @end
